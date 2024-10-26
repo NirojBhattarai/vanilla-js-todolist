b694e870a4c0ad852215337203318f674e63a043
NirojBhattarai <neerajbhttri@gmail.com>
2024-10-26 15:58:39 +0545

    Design frontend and integrate function to Add Tasks within array

---
+++ main.js
@@ -0,0 +1,25 @@
+const todoInput = document.getElementById("todo-input");
+const addTaskButton = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
+
+let tasks = [];
+
+//Adding task to array before storinf=g it on local storage
+
+addTaskButton.addEventListener("click", () => {
+  const taskText = todoInput.value.trim();
+
+  if (taskText === "") {
+    return;
+  } else {
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+
+    tasks.push(newTask);
+    todoInput.value = "";
+    console.log(tasks);
+  }
+});
